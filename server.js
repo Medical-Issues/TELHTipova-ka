8cd3fc8f29e2ba13e37db69595ca98bc2d0d31f8
Medical-Issues <veselsky.honza@gmail.com>
2026-07-26 18:56:39 +0200

    Oprava

--- server.js
+++ server.js
@@ -84,6 +84,8 @@ app.use((req, res, next) => {
     res.setHeader('X-XSS-Protection', '1; mode=block');
     res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
     res.setHeader('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
+    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'");
     next();
 });
 
